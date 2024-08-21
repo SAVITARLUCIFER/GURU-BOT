d1825ad214451a9eda4da8a1d373f55fc49f42ea
SAVITARLUCIFER <88197679+SAVITARLUCIFER@users.noreply.github.com>
2024-08-21 20:07:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -69,10 +69,10 @@ global.APIKeys = {
 }
 
 // Sticker WM
-global.botname = process.env.BOTNAME
+global.botname = process.LUCIFER_BOT
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ'
-global.author = 'ᴳᵘʳᵘ ˢᵉⁿˢᵉⁱ'
+global.packname = 'ARAKKAL LUCIFER '
+global.author = 'ARAKKAL LUCIFER '
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/guru.json\n'
 global.dygp = 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp'
